15e6482330fda0eae1edc0d07a664ee7b66f5dfd
Didier Martínez <didier.martinez@sinco.com.co>
2025-10-22 15:36:06 -0500

    Fix FizzBuzz logic to ensure "Fizz" is returned for multiples of 3

--- katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
+++ katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
@@ -8,6 +8,7 @@ namespace FizzBuzz
                 .Select(n =>
                 {
                     if (n % 15 == 0) return "FizzBuzz";
+                    if (n % 3 == 0) return "Fizz";
                     if (n % 5 == 0) return "Buzz";
                     return n.ToString();
                 })
