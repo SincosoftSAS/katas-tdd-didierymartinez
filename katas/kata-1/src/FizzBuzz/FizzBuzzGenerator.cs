518744904974f0db98f6174893deff9acd142b51
Didier Martinez <didierymartinez@gmail.com>
2025-12-15 21:18:49 -0500

    Refactor FizzBuzzGenerator and update tests to handle empty input case

--- katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
+++ katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
@@ -4,29 +4,7 @@ namespace FizzBuzz
     {
         public static List<string> Generate(int count)
         {
-            var result = new List<string>();
-
-            for (int i = 1; i <= count; i++)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    result.Add("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    result.Add("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    result.Add("Buzz");
-                }
-                else
-                {
-                    result.Add(i.ToString());
-                }
-            }
-
-            return result;
+            throw new NotImplementedException();
         }
     }
 }
--- katas/kata-1/tests/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
+++ katas/kata-1/tests/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
@@ -6,25 +6,12 @@ namespace FizzBuzz.Tests
     public class FizzBuzzGeneratorTests
     {
         [Fact]
-        public void Generate_WithOne_ReturnsListWithOne()
+        public void GenerarConElNumero0DebeRetornarUnaListaVacia()
         {
-            // Arrange & Act
-            var result = FizzBuzzGenerator.Generate(1);
-            
-            // Assert
-            result.Should().HaveCount(1);
-            result[0].Should().Be("1");
+            var result = FizzBuzzGenerator.Generate(0);
+
+            result.Should().BeEmpty();
         }
-        
-        [Fact]
-        public void Generate_WithFifteen_ReturnsListWithFizzBuzz()
-        {
-            // Arrange & Act
-            var result = FizzBuzzGenerator.Generate(15);
-            
-            // Assert
-            result.Should().HaveCount(15);
-            result[14].Should().Be("FizzBuzz");
-        }
+
     }
 }
